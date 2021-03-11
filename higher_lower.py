a25678d5167d288b8fb7d576503202988e0fb652
Rafael Moreci <79222289+Terexes@users.noreply.github.com>
2021-03-11 10:27:56 -0300

    Update higher_lower.py

--- higher_lower.py
+++ higher_lower.py
@@ -1,369 +1,84 @@
+from game_data import data
 import random
-data = [
-    {
-        'name': 'Instagram',
-        'follower_count': 346,
-        'description': 'Social media platform',
-        'country': 'United States'
-    },
-    {
-        'name': 'Cristiano Ronaldo',
-        'follower_count': 215,
-        'description': 'Footballer',
-        'country': 'Portugal'
-    },
-    {
-        'name': 'Ariana Grande',
-        'follower_count': 183,
-        'description': 'Musician and actress',
-        'country': 'United States'
-    },
-    {
-        'name': 'Dwayne Johnson',
-        'follower_count': 181,
-        'description': 'Actor and professional wrestler',
-        'country': 'United States'
-    },
-    {
-        'name': 'Selena Gomez',
-        'follower_count': 174,
-        'description': 'Musician and actress',
-        'country': 'United States'
-    },
-    {
-        'name': 'Kylie Jenner',
-        'follower_count': 172,
-        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
-        'country': 'United States'
-    },
-    {
-        'name': 'Kim Kardashian',
-        'follower_count': 167,
-        'description': 'Reality TV personality and businesswoman',
-        'country': 'United States'
-    },
-    {
-        'name': 'Lionel Messi',
-        'follower_count': 149,
-        'description': 'Footballer',
-        'country': 'Argentina'
-    },
-    {
-        'name': 'Beyoncé',
-        'follower_count': 145,
-        'description': 'Musician',
-        'country': 'United States'
-    },
-    {
-        'name': 'Neymar',
-        'follower_count': 138,
-        'description': 'Footballer',
-        'country': 'Brasil'
-    },
-    {
-        'name': 'National Geographic',
-        'follower_count': 135,
-        'description': 'Magazine',
-        'country': 'United States'
-    },
-    {
-        'name': 'Justin Bieber',
-        'follower_count': 133,
-        'description': 'Musician',
-        'country': 'Canada'
-    },
-    {
-        'name': 'Taylor Swift',
-        'follower_count': 131,
-        'description': 'Musician',
-        'country': 'United States'
-    },
-    {
-        'name': 'Kendall Jenner',
-        'follower_count': 127,
-        'description': 'Reality TV personality and Model',
-        'country': 'United States'
-    },
-    {
-        'name': 'Jennifer Lopez',
-        'follower_count': 119,
-        'description': 'Musician and actress',
-        'country': 'United States'
-    },
-    {
-        'name': 'Nicki Minaj',
-        'follower_count': 113,
-        'description': 'Musician',
-        'country': 'Trinidad and Tobago'
-    },
-    {
-        'name': 'Nike',
-        'follower_count': 109,
-        'description': 'Sportswear multinational',
-        'country': 'United States'
-    },
-    {
-        'name': 'Khloé Kardashian',
-        'follower_count': 108,
-        'description': 'Reality TV personality and businesswoman',
-        'country': 'United States'
-    },
-    {
-        'name': 'Miley Cyrus',
-        'follower_count': 107,
-        'description': 'Musician and actress',
-        'country': 'United States'
-    },
-    {
-        'name': 'Katy Perry',
-        'follower_count': 94,
-        'description': 'Musician',
-        'country': 'United States'
-    },
-    {
-        'name': 'Kourtney Kardashian',
-        'follower_count': 90,
-        'description': 'Reality TV personality',
-        'country': 'United States'
-    },
-    {
-        'name': 'Kevin Hart',
-        'follower_count': 89,
-        'description': 'Comedian and actor',
-        'country': 'United States'
-    },
-    {
-        'name': 'Ellen DeGeneres',
-        'follower_count': 87,
-        'description': 'Comedian',
-        'country': 'United States'
-    },
-    {
-        'name': 'Real Madrid CF',
-        'follower_count': 86,
-        'description': 'Football club',
-        'country': 'Spain'
-    },
-    {
-        'name': 'FC Barcelona',
-        'follower_count': 85,
-        'description': 'Football club',
-        'country': 'Spain'
-    },
-    {
-        'name': 'Rihanna',
-        'follower_count': 81,
-        'description': 'Musician and businesswoman',
-        'country': 'Barbados'
-    },
-    {
-        'name': 'Demi Lovato',
-        'follower_count': 80,
-        'description': 'Musician and actress',
-        'country': 'United States'
-    },
-    {
-        'name': "Victoria's Secret",
-        'follower_count': 69,
-        'description': 'Lingerie brand',
-        'country': 'United States'
-    },
-    {
-        'name': 'Zendaya',
-        'follower_count': 68,
-        'description': 'Actress and musician',
-        'country': 'United States'
-    },
-    {
-        'name': 'Shakira',
-        'follower_count': 66,
-        'description': 'Musician',
-        'country': 'Colombia'
-    },
-    {
-        'name': 'Drake',
-        'follower_count': 65,
-        'description': 'Musician',
-        'country': 'Canada'
-    },
-    {
-        'name': 'Chris Brown',
-        'follower_count': 64,
-        'description': 'Musician',
-        'country': 'United States'
-    },
-    {
-        'name': 'LeBron James',
-        'follower_count': 63,
-        'description': 'Basketball player',
-        'country': 'United States'
-    },
-    {
-        'name': 'Vin Diesel',
-        'follower_count': 62,
-        'description': 'Actor',
-        'country': 'United States'
-    },
-    {
-        'name': 'Cardi B',
-        'follower_count': 67,
-        'description': 'Musician',
-        'country': 'United States'
-    },
-    {
-        'name': 'David Beckham',
-        'follower_count': 82,
-        'description': 'Footballer',
-        'country': 'United Kingdom'
-    },
-    {
-        'name': 'Billie Eilish',
-        'follower_count': 61,
-        'description': 'Musician',
-        'country': 'United States'
-    },
-    {
-        'name': 'Justin Timberlake',
-        'follower_count': 59,
-        'description': 'Musician and actor',
-        'country': 'United States'
-    },
-    {
-        'name': 'UEFA Champions League',
-        'follower_count': 58,
-        'description': 'Club football competition',
-        'country': 'Europe'
-    },
-    {
-        'name': 'NASA',
-        'follower_count': 56,
-        'description': 'Space agency',
-        'country': 'United States'
-    },
-    {
-        'name': 'Emma Watson',
-        'follower_count': 56,
-        'description': 'Actress',
-        'country': 'United Kingdom'
-    },
-    {
-        'name': 'Shawn Mendes',
-        'follower_count': 57,
-        'description': 'Musician',
-        'country': 'Canada'
-    },
-    {
-        'name': 'Virat Kohli',
-        'follower_count': 55,
-        'description': 'Cricketer',
-        'country': 'India'
-    },
-    {
-        'name': 'Gigi Hadid',
-        'follower_count': 54,
-        'description': 'Model',
-        'country': 'United States'
-    },
-    {
-        'name': 'Priyanka Chopra Jonas',
-        'follower_count': 53,
-        'description': 'Actress and musician',
-        'country': 'India'
-    },
-    {
-        'name': '9GAG',
-        'follower_count': 52,
-        'description': 'Social media platform',
-        'country': 'China'
-    },
-    {
-        'name': 'Ronaldinho',
-        'follower_count': 51,
-        'description': 'Footballer',
-        'country': 'Brasil'
-    },
-    {
-        'name': 'Maluma',
-        'follower_count': 50,
-        'description': 'Musician',
-        'country': 'Colombia'
-    },
-    {
-        'name': 'Camila Cabello',
-        'follower_count': 49,
-        'description': 'Musician',
-        'country': 'Cuba'
-    },
-    {
-        'name': 'NBA',
-        'follower_count': 47,
-        'description': 'Club Basketball Competition',
-        'country': 'United States'
-    }
-]
-score = 0
 
-# Generate random accound.
-def random_account():
+
+
+def get_account():
+  """Get a account from game_data"""
   return random.choice(data)
 
-# Format data.
-def data_format(account):
-  name = account["name"]
-  description = account["description"]
-  country = account["country"]
+
+def format_data(account):
+  """Format data to give it more readability"""
+  name = account['name']
+  description = account['description']
+  country = account['country']
   return f"{name} is a {description} from {country}."
 
-# Get accounts number of followers.
+
 def get_followers(account):
   return account['follower_count']
-  
-# Check answer.
-def answer_check(answer):
+
+
+def check_guess(guess, followers_a, followers_b):
+  """Check guessed answer returning a boolean value"""
   if followers_a > followers_b:
-    return answer == 'a'
+    return guess == 'a'
   else:
     if followers_b > followers_a:
-      return answer == 'b'
-# Control score:
-def get_score(answer,score = 0):  
-  if answer:
+      return guess == 'b'
+
+
+def score_control(guess, score = 0):
+  """Gives feedback of score based on right guesses"""
+  if guess:
     score += 1
     return score
   else:
     return score
 
-
-
-keep_playing = False
-while not keep_playing:  
-  account_a = random_account()
-  account_b = random_account()
+def game(account_a, account_b, score):
+  """Runs the game"""
   if account_a == account_b:
-    account_b = random_account()
-
+    account_b = get_account()
+  
   followers_a = get_followers(account_a)
   followers_b = get_followers(account_b)
 
-# Get user guess.
-  print(f"Compare A: {data_format(account_a)}\n")
-  print(f"Against B: {data_format(account_b)}\n")
+  # Get user guess:
+  print(f"Compare A: {format_data(account_a)}\n")
+  print(f"Against B: {format_data(account_b)}\n")
   guess = input("Who have more followers? Type 'A' or 'B': ").lower()
 
-# Check user guess.
-  answer_check(guess)
+
+  # Give feedback from guesses:
+  ## Check guess:
+  checker = check_guess(guess, followers_a, followers_b)
+  if not checker:
+   # Gives player feedback  
+    print(f"You guessed wrong ! Final score: {score}")
+    return
+  else:
+   # Score keeping:
+   ## Temporary variable (temp) to store score_control value 
+   temp = score_control(guess)
+   score += temp
+   print(f'Score: {score}')      
+  
+   continue_game = input('Keep playing? "Y" or "N": \n').lower   
+   if continue_game == 'n':
+    print("Thanks for playing !")
+    return
+   else:
+    account_a = account_b
+    account_b = get_account()
+    return game(account_a, account_b, score)
   
 
-# score tracker.
-  temp = get_score(answer_check(guess))
-  score += temp
-  if not answer_check(guess):
-    print(f"Final score: {score} ")
-    break
-  print(f"Your score: {score} ")
+account_a = get_account()
+account_b = get_account()
 
-# Check to continue the game.
-  user_input = input("Do you want to keep playing? Type 'Y' or 'N': ").lower()
-  if user_input == 'n':
-    break
-  
+
+keep_playing = False
+score = 0
+game(account_a, account_b, score)
